0ab41d417413fad03b208d3b0294fcf2933d4399
Saravanan Seenivasan <s.sarav16@gmail.com>
2020-12-23 21:45:12 +0530

    Create PasswordException.java

---
+++ PasswordException.java
@@ -0,0 +1,10 @@
+
+public class PasswordException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public PasswordException(String exceptionMessage) {
+		super(exceptionMessage);
+	}
+
+}
